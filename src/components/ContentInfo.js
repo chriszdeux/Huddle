7855465fe14f3557cc2cf2218c3f2acae382d1c1
chriszdeux <chriszdeux@gmail.com>
2020-11-26 18:59:27 -0800

    change

--- src/components/ContentInfo.js
+++ src/components/ContentInfo.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
+import '../styles/styles-components/ContentInfo-style.css';
 import growTogether from '../utils/images/illustration-grow-together.svg';
 export const ContentInfo = () => {
   return (
     <>
-      <section>
-        <figure>
+      <section className="content">
+        <figure className="content--image">
           {/* <img src={ growTogether } alt="" /> */}
         </figure>
-        <article>
+        <article className="content--info">
           <h3>grow together</h3>
           <p>
             Generate meaningful discussions with yout audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form
