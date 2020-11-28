d9b4f021e607c0ee5327409f1766b7d9b54d0989
chriszdeux <chriszdeux@gmail.com>
2020-11-27 20:18:00 -0800

    ContentInfo now is dynamic :)

--- src/data/ContentInfo-data.js
+++ src/data/ContentInfo-data.js
@@ -1,10 +1,43 @@
-
+import growImg from '../utils/images/illustration-grow-together.svg';
+import flowingImg from '../utils/images/illustration-flowing-conversation.svg';
+import yourUsers from '../utils/images/illustration-your-users.svg';
 
 export const dataBase = [
   {
     id: 1,
     title: 'grow together',
     content: 'Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form',
-    img: '../utils/images/illustration-grow-together.svg',
-  }
+    img: growImg,
+  },
+
+  {
+    id: 2,
+    title: 'flowing conversations',
+    content: "You wouldn't paginate a conversation in a real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
+    img: flowingImg,
+  },
+
+  {
+    id: 3,
+    title: 'your users',
+    content: "It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in ot your app, your users can start chatting immediately.",
+    img: yourUsers,
+  },
+
+  {
+    id: 4,
+    title: 'prueba de ardilla',
+    content: "esto es una prueba para el amor de mi vida que es la ardilla muy rabiosa, me enamore? bastante, fue una locura? por supuesto/",
+    img: growImg,
+  },
+
+  {
+    id: 5,
+    title: 'soy ardilla perruna',
+    content: "me encanta rascarme la nariz, a mi esposa le desargada, pero aun asi, ella me ama :D",
+    img: flowingImg,
+  },
+
+
+
 ]
