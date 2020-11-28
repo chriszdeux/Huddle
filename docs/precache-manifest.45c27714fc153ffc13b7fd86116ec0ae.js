ff5ebd0a3062afae4d8e11bab08a3fc1cd6e6c20
chriszdeux <chriszdeux@gmail.com>
2020-11-27 21:00:46 -0800

    fixing img

--- docs/precache-manifest.45c27714fc153ffc13b7fd86116ec0ae.js
+++ docs/precache-manifest.45c27714fc153ffc13b7fd86116ec0ae.js
@@ -1,70 +1,70 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "05796ee5f5f59d8494313a8b7ee5b585",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "eddc7892e659decfdd81",
-    "url": "/static/css/main.42cfcb89.chunk.css"
+    "url": "./static/css/main.42cfcb89.chunk.css"
   },
   {
     "revision": "940fac613874f9bf7c0d",
-    "url": "/static/js/2.a8303887.chunk.js"
+    "url": "./static/js/2.a8303887.chunk.js"
   },
   {
     "revision": "f5fc2afae58c4ffc30c6148864765d4b",
-    "url": "/static/js/2.a8303887.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.a8303887.chunk.js.LICENSE.txt"
   },
   {
     "revision": "eddc7892e659decfdd81",
-    "url": "/static/js/main.b1554470.chunk.js"
+    "url": "./static/js/main.b1554470.chunk.js"
   },
   {
     "revision": "a614f30cf78e786f573c",
-    "url": "/static/js/runtime-main.ee93f85d.js"
+    "url": "./static/js/runtime-main.ee93f85d.js"
   },
   {
     "revision": "5b214dd4ae5263c0c58e1c351c6b4c03",
-    "url": "/static/media/bg-hero-desktop.5b214dd4.svg"
+    "url": "./static/media/bg-hero-desktop.5b214dd4.svg"
   },
   {
     "revision": "db68da702e9782cdab4b74d30b0f72d0",
-    "url": "/static/media/bg-hero-mobile.db68da70.svg"
+    "url": "./static/media/bg-hero-mobile.db68da70.svg"
   },
   {
     "revision": "f0f5b24aa6e8f6c57312f24da96ebf6d",
-    "url": "/static/media/icon-email.f0f5b24a.svg"
+    "url": "./static/media/icon-email.f0f5b24a.svg"
   },
   {
     "revision": "0f65814bd9f282828254e3382e223a57",
-    "url": "/static/media/icon-location.0f65814b.svg"
+    "url": "./static/media/icon-location.0f65814b.svg"
   },
   {
     "revision": "47e54a44b879380ff8adb31f40249296",
-    "url": "/static/media/icon-phone.47e54a44.svg"
+    "url": "./static/media/icon-phone.47e54a44.svg"
   },
   {
     "revision": "6641e825f006d463db7028607e3ee994",
-    "url": "/static/media/illustration-flowing-conversation.6641e825.svg"
+    "url": "./static/media/illustration-flowing-conversation.6641e825.svg"
   },
   {
     "revision": "bb27da81d1984faa5ef57668534ff1a5",
-    "url": "/static/media/illustration-grow-together.bb27da81.svg"
+    "url": "./static/media/illustration-grow-together.bb27da81.svg"
   },
   {
     "revision": "88090acec5276438b135a468364edf12",
-    "url": "/static/media/illustration-mockups.88090ace.svg"
+    "url": "./static/media/illustration-mockups.88090ace.svg"
   },
   {
     "revision": "8a5af9952fb5ed23779d589621b108a5",
-    "url": "/static/media/illustration-your-users.8a5af995.svg"
+    "url": "./static/media/illustration-your-users.8a5af995.svg"
   },
   {
     "revision": "0f7b9ab773a936b6942c1a44214b4abf",
-    "url": "/static/media/logo-facebook.0f7b9ab7.svg"
+    "url": "./static/media/logo-facebook.0f7b9ab7.svg"
   },
   {
     "revision": "3cb867305e954abaa1a8b0dd7a4e7c06",
-    "url": "/static/media/logo.3cb86730.svg"
+    "url": "./static/media/logo.3cb86730.svg"
   }
 ]);
