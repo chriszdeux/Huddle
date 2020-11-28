d406bb82d852e414bf257fc1db766df544b45417
chriszdeux <chriszdeux@gmail.com>
2020-11-27 20:18:00 -0800

    ContentInfo is now dynamic :)

--- src/components/ContentInfo.js
+++ src/components/ContentInfo.js
@@ -1,49 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../styles/styles-components/ContentInfo-style.css';
 import growTogether from '../utils/images/illustration-grow-together.svg';
 import flowingConversations from '../utils/images/illustration-flowing-conversation.svg';
 import yourUsers from '../utils/images/illustration-your-users.svg';
 
+import { dataBase } from '../data/ContentInfo-data';
 
-export const ContentInfo = () => {
+export const ContentInfo = (  ) => {
+  // const { id, title, img, content } = dataBase;
+  // debugger
+
+  const changePosition = 'content content--inverse';
+  const normalPosition = 'content'
   return (
     <>
-      <section className="content">
-        <figure className="content--image">
-          <img src={ growTogether } alt="" />
-        </figure>
-        <article className="content--info">
-          <h3>grow together</h3>
-          <p>
-            Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form
-          </p>
-        </article>
-      </section>
+      {
+        dataBase.map( item => {
+          const { id, title, img, content } = item;
+          
+          let newSet = '';
+          if (id%2==0) {
+            newSet = normalPosition;
+            // debugger
+          } else {
+            newSet = changePosition;
+          }
 
-      <section className="content content--inverse">
-        <figure className="content--image">
-          <img src={ flowingConversations } alt="" />
-        </figure>
-        <article className="content--info">
-          <h3>flowing conversations</h3>
-          <p>
-            You wouldn't paginate a conversation in a real life, so why do it online? Our threads have just-in-time loading for a more natural flow.
-          </p>
-        </article>
-      </section>
-
-      <section className="content">
-        <figure className="content--image">
-          <img src={ yourUsers } alt="" />
-        </figure>
-        <article className="content--info">
-          <h3>your users</h3>
-          <p>
-            It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in ot your app, your users can start chatting immediately.
-          </p>
-        </article>
-      </section>
+          return (
+            <>
+            <section className={newSet }>
+              
+              <figure className="content--image" >
+                <img src={ img } alt={ title } />
+      
+              </figure>
+              <article className="content--info">
+                  <h3>{ title }</h3>
+                <p>
+                  { content }
+                </p>
+              </article>
+            </section>
+          </>
+          )
+        })
+      }
     </>
   )
+  // return (
+  //   <>
+  //     <section className="content">
+  //       <figure className="content--image">
+  //         <img src={ dataBase[0].img } alt="" />
+      
+  //       </figure>
+  //       <article className="content--info">
+  //         <h3>grow together</h3>
+  //         <p>
+  //           Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form
+  //         </p>
+  //       </article>
+  //     </section>
+
+  //     {/* <section className="content content--inverse">
+  //       <figure className="content--image">
+  //         <img src={ flowingConversations } alt="" />
+  //       </figure>
+  //       <article className="content--info">
+  //         <h3>flowing conversations</h3>
+  //         <p>
+  //           You wouldn't paginate a conversation in a real life, so why do it online? Our threads have just-in-time loading for a more natural flow.
+  //         </p>
+  //       </article>
+  //     </section> */}
+
+      
+  //   </>
+  // )
+  
 }
